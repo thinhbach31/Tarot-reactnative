a714696581cf022da931914e4501fa3ee96f06f1
Thinh <thinh@Thinhs-MBP.crystal-techs.com>
2019-09-06 10:40:22 +0700

    init pj

---
+++ source/App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment, Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <SafeAreaView />
+        <View style={{ flex: 1, backgroundColor: 'black' }}>
+
+        </View>
+      </View>
+    );
+  }
+
+};
+
+export default App;
---
+++ source/screen/SpreadDetail.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import {
+    View,
+} from 'react-native'
+
+class SpreadDetail extends Component{
+
+}
+
+export default SpreadDetail
